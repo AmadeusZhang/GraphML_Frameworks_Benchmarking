5e75b6d0bb1ddf0c488845c09b05ca3308f82b3e
LeoGori <leonardogori@hotmail.it>
2022-12-28 18:33:15 +0100

    Added models and code for training on cora
    Added official DGL-pytorch implementations of GCN, GAT and (altered) GraphSAGE + code to train the models on Cora dataset

---
+++ DGL/models/gcn/pygcn.py
@@ -0,0 +1,29 @@
+'''
+Credits: https://github.com/dmlc/dgl/blob/master/examples/pytorch/gcn/train.py
+'''
+
+import dgl.nn.pytorch.conv as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+from training_hyperparameters import gcnHG
+
+
+class GCN(nn.Module):
+    def __init__(self, in_size, hid_size, out_size):
+        super().__init__()
+        self.metrics = gcnHG()
+        self.layers = nn.ModuleList()
+        # two-layer GCN
+        self.layers.append(
+            dglnn.GraphConv(in_size, hid_size, activation=F.relu)
+        )
+        self.layers.append(dglnn.GraphConv(hid_size, out_size))
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, g, features):
+        h = features
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                h = self.dropout(h)
+            h = layer(g, h)
+        return h
