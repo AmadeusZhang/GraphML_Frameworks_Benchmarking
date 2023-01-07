a5ed0f7a439809c452006a1b1d3f18540048a29c
Zijie Zhang <zijie.zhang@mail.polimi.it>
2023-01-07 11:04:16 +0100

    modified sg_runner

--- StellarGraph/models/stellargraph_runner.py
+++ StellarGraph/models/stellargraph_runner.py
@@ -2,9 +2,10 @@
 from metrics import *
 from constants import *
 from model_runner import ModelRunner
-# from stellargraph_utils import *
+from stellargraph_utils import *
 
 # Load external modules
+import os
 import networkx as nx
 import stellargraph
 from stellargraph import StellarGraph
@@ -19,9 +20,9 @@ this_folder_path = os.path.dirname( os.path.abspath( __file__ ) )
 
 class Data( object ) :
     SG = None
-    trainmask = None
-    testmask = None
-    valmask = None
+    train_mask = None
+    test_mask = None
+    val_mask = None
 
 class SGModelRunner( ModelRunner ) :
     
@@ -31,8 +32,8 @@ class SGModelRunner( ModelRunner ) :
         label = 'class_label'
 
         # Delete unused graph attributes
-        # delete_graph_attributes( self.G )
-        # print(self.G.graph, type(self.G.graph))
+        delete_graph_attributes( self.G )
+        print(self.G.graph, type(self.G.graph))
 
         print("Loading x...")
 
@@ -49,9 +50,10 @@ class SGModelRunner( ModelRunner ) :
         train_mask = [ attr["train_mask.0"] for key, attr in list(self.G.nodes(data=True)) ]
         test_mask = [ attr["test_mask.0"] for key, attr in list(self.G.nodes(data=True)) ]
         val_mask = [ attr["validation_mask.0"] for key, attr in list(self.G.nodes(data=True)) ]
-        sg_data.trainmask = node_subjects[train_mask]
-        sg_data.testmask = node_subjects[test_mask]
-        sg_data.valmask = node_subjects[val_mask]
+
+        sg_data.train_mask = node_subjects[train_mask]
+        sg_data.test_mask = node_subjects[test_mask]
+        sg_data.val_mask = node_subjects[val_mask]
 
         # delete unused attributes
         from copy import deepcopy
@@ -68,24 +70,26 @@ class SGModelRunner( ModelRunner ) :
         sg_data.SG = SG
         self.data = sg_data
 
+    def create_models(self):
+        create_gcn(self)
+        create_graph_sage(self)
+        create_gat(self)
     
     def train( self, model: Models, split_num: int = 0 ) :
         # Split data
         # TODO: random split
 
         pass
-    
-    def create_models( self ) :
-        create_gcn( self )
-        create_graph_sage( self )
-        create_gat( self )
+
 
 def create_gcn( modelrunner: SGModelRunner ) :
     from stellargraph.layer import GCN
     hidden_dim = Models.GCN.params.hidden_size
     dropout = Models.GCN.params.dropout
+
     # define generator
     generator = FullBatchNodeGenerator( modelrunner.data.SG, method="gcn" )
+
     # create the GCN model
     gcn = GCN(
         layer_sizes=[hidden_dim, hidden_dim], # num_layers = 2
@@ -103,9 +107,11 @@ def create_gcn( modelrunner: SGModelRunner ) :
         loss=losses.categorical_crossentropy,
         metrics=["acc"],
     )
+
     # export model summary if needed
     # with open('model_summary.txt', 'w') as f:
     #     model.summary( print_fn=lambda x: f.write(x + '\n') )
+
     # save model to class
     modelrunner.models[Models.GCN] = model
 
@@ -114,10 +120,12 @@ def create_graph_sage( modelrunner: SGModelRunner ) :
     from stellargraph.layer import GraphSAGE
     hidden_dim = Models.GRAPHSAGE.params.hidden_size
     dropout = Models.GRAPHSAGE.params.dropout
+
     # define generator
     num_nodes = len( modelrunner.data.SG.nodes() )
     num_samples = [10, 5] # FIXME: or guarantee a motivation for this choice
     generator = GraphSAGENodeGenerator( modelrunner.data.SG, batch_size=num_nodes, num_samples=num_samples )
+
     # create the GraphSAGE model
     graphsage = GraphSAGE(
         layer_sizes=[hidden_dim, hidden_dim], # num_layers = 2
@@ -127,6 +135,7 @@ def create_graph_sage( modelrunner: SGModelRunner ) :
     )
     x_inp, x_out = graphsage.in_out_tensors()
     predictions = layers.Dense( units=modelrunner.num_classes, activation="softmax" )( x_out )
+
     # create the keras model
     model = Model( inputs=x_inp, outputs=predictions )
     model.compile(
@@ -134,9 +143,11 @@ def create_graph_sage( modelrunner: SGModelRunner ) :
         loss=losses.categorical_crossentropy,
         metrics=["acc"],
     )
+
     # export model summary if needed
     # with open('model_summary.txt', 'w') as f:
     #     model.summary( print_fn=lambda x: f.write(x + '\n') )
+
     # save model to class
     modelrunner.models[Models.GRAPHSAGE] = model
 
@@ -145,8 +156,10 @@ def create_gat( modelrunner: SGModelRunner ) :
     from stellargraph.layer import GAT
     hidden_dim = Models.GAT.params.hidden_size
     dropout = Models.GAT.params.dropout
+
     # define generator
     generator = FullBatchNodeGenerator( modelrunner.data.SG, method="gat" )
+
     # create the GAT model
     gat = GAT(
         layer_sizes=[hidden_dim, hidden_dim], # num_layers = 2
@@ -159,6 +172,7 @@ def create_gat( modelrunner: SGModelRunner ) :
     )
     x_inp, x_out = gat.in_out_tensors()
     predictions = layers.Dense( units=modelrunner.num_classes, activation="softmax" )( x_out )
+
     # create the keras model
     model = Model( inputs=x_inp, outputs=predictions )
     model.compile(
@@ -166,8 +180,10 @@ def create_gat( modelrunner: SGModelRunner ) :
         loss=losses.categorical_crossentropy,
         metrics=["acc"],
     )
+
     # export model summary if needed
     # with open('model_summary.txt', 'w') as f:
     #     model.summary( print_fn=lambda x: f.write(x + '\n') )
+
     # save model to class
     modelrunner.models[Models.GAT] = model
